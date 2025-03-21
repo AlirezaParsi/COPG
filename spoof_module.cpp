f88f04cbcf24330f9f4c2106874f5c0689890263
Aliza <alirezanajmabadi79@gmail.com>
2025-03-21 12:32:08 +0330

    Update spoof_module.cpp

--- spoof_module.cpp
+++ spoof_module.cpp
@@ -1,23 +1,39 @@
-#include <sys/types.h>
-#include <zygisk.hh>
-#include <android/log.h>
-#include <dlfcn.h>
+#include <jni.h>
 #include <string>
-#include <sys/system_properties.h>
+#include <android/log.h>
+#include <zygisk.hh>
 #include <nlohmann/json.hpp>
 #include <fstream>
-#include <unordered_set>
+#include <unordered_map>
 
 #define LOG_TAG "SpoofModule"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 
 using json = nlohmann::json;
 
+struct DeviceInfo {
+    std::string brand;
+    std::string device;
+    std::string manufacturer;
+    std::string model;
+};
+
 class SpoofModule : public zygisk::ModuleBase {
 public:
     void onLoad(zygisk::Api* api, JNIEnv* env) override {
         this->api = api;
         this->env = env;
+
+        buildClass = env->FindClass("android/os/Build");
+        if (buildClass) {
+            modelField = env->GetStaticFieldID(buildClass, "MODEL", "Ljava/lang/String;");
+            brandField = env->GetStaticFieldID(buildClass, "BRAND", "Ljava/lang/String;");
+            deviceField = env->GetStaticFieldID(buildClass, "DEVICE", "Ljava/lang/String;");
+            manufacturerField = env->GetStaticFieldID(buildClass, "MANUFACTURER", "Ljava/lang/String;");
+        } else {
+            LOGD("Failed to find android/os/Build class");
+        }
+
         loadConfig();
     }
 
@@ -28,36 +44,37 @@ public:
         }
 
         const char* package_name = env->GetStringUTFChars(args->nice_name, nullptr);
-        if (package_set.find(package_name) == package_set.end()) {
-            LOGD("Skipping non-target package: %s", package_name);
+        if (package_map.find(package_name) == package_map.end()) {
             api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
-        } else {
-            char abi[PROP_VALUE_MAX];
-            __system_property_get("ro.product.cpu.abi", abi);
-            const char* gadget_path = strstr(abi, "arm64") ?
-                "/data/adb/modules/COPG/frida/libcopg-arm64.so" :
-                "/data/adb/modules/COPG/frida/libcopg-armv7-a.so";
-            
-            void* handle = dlopen(gadget_path, RTLD_NOW);
-            if (!handle) {
-                LOGD("Failed to load Frida Gadget for %s: %s", package_name, dlerror());
-            } else {
-                LOGD("Frida Gadget 16.7.0 loaded for %s", package_name);
-            }
         }
+        env->ReleaseStringUTFChars(args->nice_name, package_name);
+    }
 
+    void postAppSpecialize(const zygisk::AppSpecializeArgs* args) override {
+        if (!args || !args->nice_name || package_map.empty() || !buildClass) return;
+
+        const char* package_name = env->GetStringUTFChars(args->nice_name, nullptr);
+        if (!package_name) return;
+
+        auto it = package_map.find(package_name);
+        if (it != package_map.end()) {
+            const DeviceInfo& info = it->second;
+            spoofDevice(info.brand.c_str(), info.device.c_str(), info.manufacturer.c_str(), info.model.c_str());
+        }
         env->ReleaseStringUTFChars(args->nice_name, package_name);
     }
 
 private:
     zygisk::Api* api;
     JNIEnv* env;
-    std::unordered_set<std::string> package_set;
+    std::unordered_map<std::string, DeviceInfo> package_map;
+    jclass buildClass = nullptr;
+    jfieldID modelField = nullptr, brandField = nullptr, deviceField = nullptr, manufacturerField = nullptr;
 
     void loadConfig() {
         std::ifstream file("/data/adb/modules/COPG/config.json");
         if (!file.is_open()) {
-            LOGD("Failed to open config.json");
+            LOGD("Failed to open config.json - please update via action script");
             return;
         }
 
@@ -65,16 +82,35 @@ private:
             json config = json::parse(file);
             for (auto& [key, value] : config.items()) {
                 if (key.find("_DEVICE") != std::string::npos) continue;
-                for (const auto& pkg : value.get<std::vector<std::string>>()) {
-                    package_set.insert(pkg);
+
+                auto packages = value.get<std::vector<std::string>>();
+                std::string device_key = key + "_DEVICE";
+                if (!config.contains(device_key)) continue;
+
+                auto device = config[device_key];
+                DeviceInfo info {
+                    device["BRAND"].get<std::string>(),
+                    device["DEVICE"].get<std::string>(),
+                    device["MANUFACTURER"].get<std::string>(),
+                    device["MODEL"].get<std::string>()
+                };
+
+                for (const auto& pkg : packages) {
+                    package_map[pkg] = info;
                 }
             }
-            LOGD("Loaded %zu target packages", package_set.size());
+            LOGD("Loaded %zu package mappings", package_map.size());
         } catch (const json::exception& e) {
             LOGD("JSON parse error: %s", e.what());
         }
-
         file.close();
+    }
+
+    void spoofDevice(const char* brand, const char* device, const char* manufacturer, const char* model) {
+        if (modelField) env->SetStaticObjectField(buildClass, modelField, env->NewStringUTF(model));
+        if (brandField) env->SetStaticObjectField(buildClass, brandField, env->NewStringUTF(brand));
+        if (deviceField) env->SetStaticObjectField(buildClass, deviceField, env->NewStringUTF(device));
+        if (manufacturerField) env->SetStaticObjectField(buildClass, manufacturerField, env->NewStringUTF(manufacturer));
     }
 };
 
